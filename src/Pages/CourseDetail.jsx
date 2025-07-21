654aa25fd801d864cd42b9952fb6785c723290a2
Adedayo John Ilesanmi <johnadedayo1@gmail.com>
2025-07-21 12:18:35 +0100

    not yet a dynamic image yet but i added an image to the hero

--- src/Pages/CourseDetail.jsx
+++ src/Pages/CourseDetail.jsx
@@ -149,12 +149,15 @@ const CourseDetail = () => {
 
   return (
     <div className="min-h-screen bg-gray-900 text-white py-20 px-6 md:px-10">
-      <div className="max-w-5xl mx-auto space-y-10">
-        <h1 className="text-4xl font-bold">{course.title}</h1>
+      <div className="w-10/12 mx-auto space-y-10">
+      <div>
+        <img src="/course-details.svg" alt="course details" />
+      </div>
+        <h1 className="text-4xl font-bold text-green-500">{course.title}</h1>
         <p className="text-lg text-gray-300">{course.description}</p>
-        <div className="flex flex-col sm:flex-row sm:items-center gap-4 text-sm text-gray-400">
-          <span>{course.currentStudents}</span>
-          <span>{course.duration}</span>
+        <div className=" flex flex-col gap-4 text-md">
+          <span className="bg-white w-80 py-2 p-2 rounded-md shadow font-medium text-black">{course.currentStudents}</span>
+          <span className="bg-white w-44 py-2 p-2 rounded-md shadow font-medium text-black">{course.duration}</span>
         </div>
         <button className="bg-[#39B54A] text-white px-8 py-3 rounded-lg font-semibold">
           {course.enrollButton}
